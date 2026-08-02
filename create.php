0004f7d569336849214fb8060f8d7648774dc67a
kareem-microsoft <microsoftgloballearning@gmail.com>
2026-08-02 22:43:50 +0300

    feat(create) add create.php for adding new student records

---
+++ create.php
@@ -0,0 +1,33 @@
+<?php
+require_once "connection.php";
+$conn = connection();
+?>
+
+<?php if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $student_id = isset($_POST["student_id"]) ? $_POST["student_id"] : 0;
+    $student_name = $student_age = "";
+
+    $student_name = htmlspecialchars($_POST["student_name"]);
+
+    if(filter_var($_POST["student_id"], FILTER_VALIDATE_INT)){
+        $student_id = (int) $_POST["student_id"];
+    }
+
+    if(filter_var($_POST["student_age"], FILTER_VALIDATE_INT)){
+        $student_age = htmlspecialchars($_POST["student_age"]);
+    }
+
+    $sql = "INSERT INTO students(student_id, student_name, student_age) VALUES ($student_id, '$student_name', $student_age)";
+    $conn->query($sql);
+    header("Location: index.php");
+}
+?>
+
+<?php if ($_SERVER["REQUEST_METHOD"] === "GET"): ?>
+    <form action="create.php" method="POST">
+        <input type="number" name="student_id" required>
+        <input type="text" name="student_name" required>
+        <input type="number" name="student_age" required>
+        <input type="submit" value="Add Student">
+    </form>
+<?php endif ?>
